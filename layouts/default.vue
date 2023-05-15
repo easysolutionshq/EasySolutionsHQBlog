d1b9061add6acc755742a8970a3f225485a0288e
easysolutionshq <127499535+easysolutionshq@users.noreply.github.com>
2023-05-16 00:14:00 +0530

    add captioncreator ad

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,9 @@
         <div class="col-span-1 md:col-span-2 px-4 order-first md:order-none">
             <SectionNavBar />
             <slot />
+            <div class="bg-green-300 text-2xl py-2 my-2">
+            Want to Create Subtitles for Your Videos for Free? Visit <NuxtLink class="font-bold text-blue-700" to="https://captioncreator.cc?utm_source=easysolutionshq&utm_medium=banner&utm_campaign=easysolutionshq">CaptionCreator.cc</NuxtLink>
+          </div>
             <SectionFooter />
         </div>
         <SectionRightSideBar/>
